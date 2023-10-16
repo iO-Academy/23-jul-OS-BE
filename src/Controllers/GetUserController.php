4d636cd86d5b53b19a895ed10fd010dc93b838d1
Cosmin <cosmin_7d@yahoo.com>
2023-10-16 16:42:31 +0100

    Added Error Handling and messages given for not numeric id

--- src/Controllers/GetUserController.php
+++ src/Controllers/GetUserController.php
@@ -3,8 +3,11 @@
 namespace CaterpillarOS\Controllers;
 
 use CaterpillarOS\Models\UserModel;
+use mysql_xdevapi\Exception;
+use PHPUnit\Event\Code\Throwable;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use function PHPUnit\Framework\throwException;
 
 class GetUserController
 {
@@ -20,12 +23,36 @@ class GetUserController
 
     public function __invoke(RequestInterface $request, ResponseInterface $response, $args): ResponseInterface
     {
-       $user = $this->userModel->getUserById($args['userId']);
-       $responseBody = [
-           'message' => "User successfully retrieved from db",
-           'status' => 200,
-           'data' => $user
-       ];
+        try{
+            if (!ctype_digit($args['userId'])) {
+                $responseBody = [
+                    'message' => "User id must be numeric",
+                    'status' => 400,
+                    'data' => []
+                ];
+            } else {
+                $user = $this->userModel->getUserById($args['userId']);
+                if($user == []) {
+                    $responseBody = [
+                        'message' => "Invalid user id",
+                        'status' => 400,
+                        'data' => []
+                    ];
+                } else {
+                    $responseBody = [
+                        'message' => "User successfully retrieved from db",
+                        'status' => 200,
+                        'data' => $user
+                    ];
+                }
+            }
+        } catch (\Exception $e) {
+            $responseBody = [
+                'message' => "Unexpected error",
+                'status' => 500,
+            ];
+        }
+
        return $response->withJson($responseBody);
     }
 }
